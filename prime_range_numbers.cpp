fe7b7a713ea199554826f0b31366efaae4011576
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2024-08-31 17:07:48 +0530

    changes done

---
+++ prime_range_numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int range,i,num,start_num,end_num;
+
+    cout << "Enter Starting and ending Numbers:";
+    cin >> start_num >> end_num;
+
+    for(num = start_num; num <= end_num; num++){
+        for(i = 2; i < num; i++){
+            if(num % i == 0){
+                break;
+            }
+        }
+        if(i == num){
+            cout << num << " ";
+        }
+    }
+    
+    return 0;
+}
